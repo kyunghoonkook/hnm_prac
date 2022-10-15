c796fcfc5fc11e2d331d7665d2337c7614fdca15
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-15 18:54:34 +0900

    url

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -10,7 +10,7 @@ const ProductAll = () => {
   const [query, setQuery] = useSearchParams();
   const getProducts = async () => {
     let searchQuery = query.get("q") || "";
-    let url = `http://localhost:3004/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/kyunghoonkook/hnm_prac/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -6,7 +6,7 @@ const ProductDetail = () => {
   let { id } = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    let url = `http://localhost:3004/products/${id}`;
+    let url = `https://my-json-server.typicode.com/kyunghoonkook/hnm_prac/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
